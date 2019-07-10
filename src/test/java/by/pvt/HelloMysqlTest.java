704dbf9882b69dcbfb7116d0273d4266033eadda
rubashkin_mikhail@tut.by <pristijpribor@mail.ru>
2019-07-10 12:12:08 +0300

    new

--- src/test/java/by/pvt/HelloMysqlTest.java
+++ src/test/java/by/pvt/HelloMysqlTest.java
@@ -9,24 +9,55 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.junit.*;
 
 
-public class HelloMysqlTest {
+public class HelloMysqlTest extends DBTestCase {
 
+    public HelloMysqlTest(String name) {
+        super(name);
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.jdbc.Driver");
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:mysql://localhost:3306/hello_mysql_junit");
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "root");
+        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "pristijpriborF1539249f");
+    }
+
+
+    @Override
+    protected IDataSet getDataSet() throws Exception {
+        return new FlatXmlDataSetBuilder().build(HelloMysqlTest.class.getResourceAsStream("system_users.xml"));
+    }
 
     @Test
-    public void testConnection(){
-        try {
-            Connection connection =
-            DriverManager.getConnection("jdbc:mysql://localhost:3306/hello_mysql?createDatabaseIfNotExist=true&amp;useSSL=false", "root", "pristijpriborF1539249f");
+    public void testConnection() {
+        try (Connection connection =
+                    DriverManager
+                            .getConnection("jdbc:mysql://localhost:3306/hello_mysql_junit?createDatabaseIfNotExist=true&amp;useSSL=false", "root", "pristijpriborF1539249f");
             PreparedStatement ps = connection.prepareStatement("select * from system_users");
+            ){
             ResultSet rs = ps.executeQuery();
             assertNotNull(rs);
 
+            int rawCount = 0;
+            int activeUser = 0;
+
+            while (rs.next()) {
+                rawCount++;
+                if (rs.getBoolean("active")) activeUser++;
+            }
+            assertEquals(4, rawCount);
+            assertEquals(2, activeUser);
+
+            rs.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
     }
 }
+
+
